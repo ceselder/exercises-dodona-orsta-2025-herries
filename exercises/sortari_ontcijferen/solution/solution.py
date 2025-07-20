5b74d42e094f64c0bf213df41e4eabfc18dc787d
Celeste <celestedeschamphelaere@protonmail.com>
2025-07-20 19:30:07 +0200

    cba commit messages

--- exercises/sortari_ontcijferen/solution/solution.py
+++ exercises/sortari_ontcijferen/solution/solution.py
@@ -1,18 +1,5 @@
-def bepaal_winnaar(bord):
-    lijnen_om_te_checken = []
-
-    for i in range(3):
-        lijnen_om_te_checken.append((bord[i][0], bord[i][1], bord[i][2])) # Rijen toevoegen
-        lijnen_om_te_checken.append((bord[0][i], bord[1][i], bord[2][i])) # Kolommen toevoegen
-
-    # Voeg diagonalen toe
-    lijnen_om_te_checken.append((bord[0][0], bord[1][1], bord[2][2]))
-    lijnen_om_te_checken.append((bord[0][2], bord[1][1], bord[2][0]))
-
-    for lijn in lijnen_om_te_checken:
-        if lijn == ("X", "X", "X"):
-            return "X wint"
-        elif lijn == ("O", "O", "O"):
-            return "O wint"
-
-    return "niemand wint"
+def ontcijfer(alien_zin, woordenboek):
+    alien_zin.sort()
+    vertaalde_woorden = [woordenboek.get(woord, '???') for woord in alien_zin]
+    vertaalde_zin = ' '.join(vertaalde_woorden)
+    print(vertaalde_zin)
